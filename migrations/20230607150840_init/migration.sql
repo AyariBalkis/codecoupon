9e8ee00e286ef83f830c7534b98f4a63c460e5bf
AyariBalkis <74027186+AyariBalkis@users.noreply.github.com>
2023-06-07 18:01:38 +0200

    Revert "second commit"

--- migrations/20230607150840_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Coupon` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `code` VARCHAR(191) NOT NULL,
-    `discount` DOUBLE NOT NULL,
-    `store` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
