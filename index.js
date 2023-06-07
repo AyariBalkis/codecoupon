70c2f6e1120ae5b8045f3639c00698e30c7db000
belki <ayari.balkis@esprit.tn>
2023-06-07 17:36:27 +0200

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const mysql = require ('mysql');
+//const mysql = require ('mysql');
 const express = require('express');
 const bodyparser = require('body-parser');
-
+const axios =require('axios');
+const{PrismaClient} = require('@prisma/client')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON request bodies
+//app.use(bodyParser.json());
 app.use(express.json());
 
+//prisma!!!!!
+const prisma = new PrismaClient();
+// Example usage of Prisma client
+async function getCoupons() {
+  const coupons = await prisma.coupon.findMany();
+  console.log(coupons);
+}
+
+// Call the function to fetch coupons
+getCoupons()
+  .catch((error) => {
+    console.error('Error retrieving coupons:', error);
+  })
+  .finally(async () => {
+    // Disconnect Prisma client
+    await prisma.$disconnect();
+  });
 // MySQL connection setup
-const connection = mysql.createConnection({
-  host: 'localhost',
+//const dbUrl = 'http://localhost:3306'; // 
+/*const connection = mysql.createConnection({
+  host:'localhost', // Replace with the IP address or hostname of the remote MySQL server
   user: 'root', // Use the appropriate username
   password: '', // Use the appropriate password
   database: 'coupon_codes',
   multipleStatements: true,
 });
 
+*/
+/*
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
@@ -25,9 +47,10 @@ connection.connect((err) => {
     console.log('Connected to the database!');
   }
 });
-
+*/
 // GET all coupons
-app.get('/coupons', (req, res) => {
+//axios.get
+/*app.get('/coupons', (req, res) => {
   connection.query('SELECT * FROM coupons', (error, results) => {
     if (error) {
       console.error('Error retrieving coupons:', error);
@@ -37,7 +60,18 @@ app.get('/coupons', (req, res) => {
     }
   });
 });
-
+*/
+// GET all coupons
+app.get('/coupons', async (req, res) => {
+  try {
+    const coupons = await prisma.coupon.findMany();
+    res.json(coupons);
+  } catch (error) {
+    console.error('Error retrieving coupons:', error);
+    res.status(500).send('Error retrieving coupons');
+  }
+});
+/*
 // GET a specific coupon
 app.get('/coupons/:id', (req, res) => {
   const couponId = req.params.id;
@@ -51,6 +85,26 @@ app.get('/coupons/:id', (req, res) => {
       res.json(results[0]);
     }
   });
+});
+*/
+// GET a specific coupon
+app.get('/coupons/:id', async (req, res) => {
+  const couponId = parseInt(req.params.id);
+  try {
+    const coupon = await prisma.coupon.findUnique({
+      where: {
+        id: parseInt(couponId),
+      },
+    });
+    if (!coupon) {
+      res.status(404).send(`Coupon with ID ${couponId} not found`);
+    } else {
+      res.json(coupon);
+    }
+  } catch (error) {
+    console.error(`Error retrieving coupon with ID ${couponId}:`, error);
+    res.status(500).send(`Error retrieving coupon with ID ${couponId}`);
+  }
 });
 
 // POST create a coupon
@@ -67,6 +121,7 @@ app.get('/coupons/:id', (req, res) => {
   });
 });
 */
+/*
 // PUT update a coupon
 app.put('/coupons/:id', (req, res) => {
   const couponId = req.params.id;
@@ -80,7 +135,25 @@ app.put('/coupons/:id', (req, res) => {
     }
   });
 });
-
+*/
+// PUT update a coupon
+app.put('/coupons/:id', async (req, res) => {
+  const couponId = parseInt(req.params.id);
+  const couponData = req.body;
+  try {
+    const updatedCoupon = await prisma.coupon.update({
+      where: {
+        id: parseInt(couponId),
+      },
+      data: couponData,
+    });
+    res.send(`Coupon with ID ${couponId} updated successfully`);
+  } catch (error) {
+    console.error('Error updating coupon:', error);
+    res.status(500).send('Error updating coupon');
+  }
+});
+/*
 // DELETE a coupon
 app.delete('/coupons/:id', (req, res) => {
   const couponId = req.params.id;
@@ -94,24 +167,61 @@ app.delete('/coupons/:id', (req, res) => {
       res.send(`Coupon with ID ${couponId} deleted successfully`);
     }
   });
+});*/
+// DELETE a coupon
+app.delete('/coupons/:id', async (req, res) => {
+  const couponId = parseInt(req.params.id);
+  try {
+    const deletedCoupon = await prisma.coupon.delete({
+      where: {
+        id: parseInt(couponId),
+      },
+    });
+    res.send(`Coupon with ID ${couponId} deleted successfully`);
+  } catch (error) {
+    console.error('Error deleting coupon:', error);
+    res.status(500).send('Error deleting coupon');
+  }
 });
+
+/*
 // POST create a coupon
 app.post('/coupons', (req, res) => {
   const couponData = req.body;
   const couponCode = generateCouponCode(); // Generate a unique coupon code
-  
+  console.log(couponCode)
   // Add the generated coupon code to the coupon data
   couponData.code = couponCode;
   
   connection.query('INSERT INTO coupons SET ?', couponData, (error, results) => {
     if (error) {
-      console.error('Error creating coupon:', error);
+      console.error('Error creating coupon:', error.message);
       res.status(500).send('Error creating coupon');
     } else {
       const newCouponId = results.insertId;
       res.status(201).send(`New coupon created with ID: ${newCouponId}`);
     }
   });
+});
+*/
+// POST create a coupon
+app.post('/coupons', async (req, res) => {
+  const couponData = req.body;
+  const couponCode = generateCouponCode(); // Generate a unique coupon code
+  console.log(couponCode);
+  // Add the generated coupon code to the coupon data
+  couponData.code = couponCode;
+
+  try {
+    const createdCoupon = await prisma.coupon.create({
+      data: couponData,
+    });
+    const newCouponId = createdCoupon.id;
+    res.status(201).send(`New coupon created with ID: ${newCouponId}`);
+  } catch (error) {
+    console.error('Error creating coupon:', error.message);
+    res.status(500).send('Error creating coupon');
+  }
 });
 
 // Function to generate a random coupon code
